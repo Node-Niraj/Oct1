5a6dd07e13db6d94551d324913963f3262912aa1
Niraj Node <nirpython@gmail.com>
2023-10-12 13:07:13 +0530

    added images

--- Component/SelectionCard.js
+++ Component/SelectionCard.js
@@ -16,19 +16,19 @@ export default function SelectionCard() {
                 </View>
                 <View style={[styles.Cards, styles.CardSel]}>
                 <Image style={styles.tinyLogo} source={{
-                        uri: "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg",
+                        uri: "https://cdn.pixabay.com/photo/2013/07/12/18/21/tea-153336_1280.png",
                     }}
                     />
                 </View>
                 <View style={[styles.Cards, styles.CardSel]}>
                 <Image style={styles.tinyLogo} source={{
-                        uri: "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg",
+                        uri: "https://cdn.pixabay.com/photo/2014/06/26/16/33/cocktail-377960_1280.png",
                     }}
                     />
                 </View>
                 <View style={[styles.Cards, styles.CardSel]}>
                 <Image style={styles.tinyLogo} source={{
-                        uri: "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg",
+                        uri: "https://cdn.pixabay.com/photo/2021/01/15/17/01/green-5919790_1280.jpg",
                     }}
                     />
                 </View>
